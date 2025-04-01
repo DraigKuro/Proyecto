f1c976c461f507915f0c200526108bab353dc385
DraigKuro <156693160+DraigKuro@users.noreply.github.com>
2025-04-01 18:36:29 +0200

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,72 +1,80 @@
-CREATE DATABASE biblioteca_db;
+-- crear la base de datos si no existe
+create database if not exists biblioteca_db;
 
-\c biblioteca_db;
+-- usar la base de datos
+use biblioteca_db;
 
-CREATE TABLE usuarios (
-    ID_usuario INT PRIMARY KEY NOT NULL,
-    nombre VARCHAR NOT NULL,
-    apellidos VARCHAR NOT NULL,
-    usuario VARCHAR NOT NULL,
-    correo VARCHAR NOT NULL,
-    pass VARCHAR NOT NULL
+-- crear la tabla usuarios si no existe
+create table if not exists usuarios (
+    id_usuario int primary key not null,
+    nombre varchar(255) not null,
+    apellidos varchar(255) not null,
+    usuario varchar(255) not null,
+    correo varchar(255) not null,
+    pass varchar(255) not null
 );
 
-CREATE TABLE info (
-    ID_info INT PRIMARY KEY NOT NULL,
-    ID_FK_usuario INT NOT NULL,
-    salt VARCHAR NOT NULL,
-    cartera DOUBLE NOT NULL,
-    FOREIGN KEY (ID_FK_usuario) REFERENCES usuarios(ID_usuario) ON DELETE CASCADE
+-- crear la tabla info si no existe
+create table if not exists info (
+    id_info int primary key not null,
+    id_fk_usuario int not null,
+    salt varchar(255) not null,
+    cartera double not null,
+    foreign key (id_fk_usuario) references usuarios(id_usuario) on delete cascade
 );
 
-CREATE TABLE biblioteca (
-    ID_biblioteca INT PRIMARY KEY NOT NULL,
-    ID_FK_usuario INT NOT NULL,
-    cantidad_libros INT NOT NULL,
-    FOREIGN KEY (ID_FK_usuario) REFERENCES usuarios(ID_usuario) ON DELETE CASCADE
+-- crear la tabla biblioteca si no existe
+create table if not exists biblioteca (
+    id_biblioteca int primary key not null,
+    id_fk_usuario int not null,
+    cantidad_libros int not null,
+    foreign key (id_fk_usuario) references usuarios(id_usuario) on delete cascade
 );
 
-CREATE TABLE libro (
-    ID_libro INT PRIMARY KEY NOT NULL,
-    titulo VARCHAR NOT NULL,
-    idioma VARCHAR[] NOT NULL,
-    genero VARCHAR[] NOT NULL,
-    fecha_publi DATE NOT NULL,
-    autores VARCHAR[] NOT NULL,
-    precio DOUBLE PRECISION NOT NULL,
-    DRM BOOLEAN NOT NULL,
-    N_paginas INT NOT NULL,
-    sinopsis VARCHAR NOT NULL,
-    valoracion VARCHAR NOT NULL,
-    URLibro VARCHAR NOT NULL,
-    URLportada VARCHAR NOT NULL
+-- crear la tabla libro si no existe
+create table if not exists libro (
+    id_libro int primary key not null,
+    titulo varchar(255) not null,
+    idioma varchar(255) not null,
+    genero varchar(255) not null,
+    fecha_publi date not null,
+    autores varchar(255) not null,
+    precio double not null,
+    drm boolean not null,
+    n_paginas int not null,
+    sinopsis varchar(255) not null,
+    valoracion varchar(255) not null,
+    urllibro varchar(255) not null,
+    urlportada varchar(255) not null
+);
+-- crear la tabla editorial si no existe
+create table if not exists editorial (
+id_editorial int primary key not null,
+nombre varchar(255) not null,
+nif varchar(255) not null,
+direccion varchar(255) not null,
+correo varchar(255) not null,
+telefono int not null,
+contacto varchar(255),
+web varchar(255)
 );
 
-CREATE TABLE editorial (
-    ID_editorial INT PRIMARY KEY NOT NULL,
-    nombre VARCHAR NOT NULL,
-    NIF VARCHAR NOT NULL,
-    direccion VARCHAR NOT NULL,
-    correo VARCHAR NOT NULL,
-    telefono INT NOT NULL,
-    contacto VARCHAR,
-    web VARCHAR
+-- crear la tabla biblio_libro si no existe
+create table if not exists biblio_libro (
+id_fk_biblioteca int not null,
+id_fk_libro int not null,
+fecha_compra date not null,
+primary key (id_fk_biblioteca, id_fk_libro),
+foreign key (id_fk_biblioteca) references biblioteca(id_biblioteca) on delete cascade,
+foreign key (id_fk_libro) references libro(id_libro) on delete cascade
 );
 
-CREATE TABLE biblio_libro (
-    ID_FK_biblioteca INT NOT NULL,
-    ID_FK_libro INT NOT NULL,
-    fecha_compra DATE NOT NULL,
-    PRIMARY KEY (ID_FK_biblioteca, ID_FK_libro),
-    FOREIGN KEY (ID_FK_biblioteca) REFERENCES biblioteca(ID_biblioteca) ON DELETE CASCADE,
-    FOREIGN KEY (ID_FK_libro) REFERENCES libro(ID_libro) ON DELETE CASCADE
-);
-
-CREATE TABLE libro_edit (
-    ID_FK_editorial INT NOT NULL,
-    ID_FK_libro INT NOT NULL,
-    fecha_alta DATE NOT NULL,
-    PRIMARY KEY (ID_FK_editorial, ID_FK_libro),
-    FOREIGN KEY (ID_FK_editorial) REFERENCES editorial(ID_editorial) ON DELETE CASCADE,
-    FOREIGN KEY (ID_FK_libro) REFERENCES libro(ID_libro) ON DELETE CASCADE
+-- crear la tabla libro_edit si no existe
+create table if not exists libro_edit (
+id_fk_editorial int not null,
+id_fk_libro int not null,
+fecha_alta date not null,
+primary key (id_fk_editorial, id_fk_libro),
+foreign key (id_fk_editorial) references editorial(id_editorial) on delete cascade,
+foreign key (id_fk_libro) references libro(id_libro) on delete cascade
 );
